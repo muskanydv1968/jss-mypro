3fbcdbe9def4c43c99a57d88b6cc050d9cfc1e81
muskanydv1968 <muskanyadavrs@gmail.com>
2026-04-11 12:11:40 +0530

    01_variable.js

---
+++ 01_variable.js
@@ -0,0 +1,10 @@
+const accountID=14555
+let accountEmail="muskan@yadav.com"
+var accountPassword="1686"
+let accountcity="Delhi"
+
+accountPassword="906864"
+accountEmail="muski@abes.com"
+accountcity="goa"
+console.log(accountID);
+console.table([accountID,accountEmail,accountPassword,accountcity])
