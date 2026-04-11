a48b0195b36fb53350c7f067d3f79c9b37d22921
muskanydv1968 <muskanyadavrs@gmail.com>
2026-04-11 17:47:16 +0530

    conversion

---
+++ 01_conversionvariable.js
@@ -0,0 +1,26 @@
+//let score=33
+//let score="33" //ye string hai 
+let score=1 
+console.log(typeof score);
+let value=Number(score) //here we converted the strig value into the number
+console.log(typeof value);
+console.log(value); //here no value is given
+
+
+//"33"=33
+//"33abs"=NaN
+//true=1 false=0
+//undifined=NaN,hitesh=Nan
+
+let islogged=1
+let booleanislogged=Boolean(islogged)
+
+console.log(booleanislogged);
+
+//1=true,0=false
+//""=false
+//"hitesh"=true
+let stringnumber=33
+let stringvalue=String(stringnumber)
+console.log(stringvalue);
+console.log(typeof stringvalue);
