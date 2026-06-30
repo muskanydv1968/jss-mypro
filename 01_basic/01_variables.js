60d4f6462ac8454288ce304e2f81034a7e227273
Muskan <muskan@Muskans-MacBook-Neo.local>
2026-06-30 13:57:26 +0530

    my first commit

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -2,4 +2,5 @@ const accountid = 14267
 let accountmail = "muskan@gmil.com";
 accountpasswoed = "2386"
 let accountstate;
+console.log("hellow")
 console.table([accountmail,accountpasswoed,accountid,accountstate])
