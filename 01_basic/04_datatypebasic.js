c696ba0a638b1aaeba2344b6902677cabea10127
Muskan <muskan@Muskans-MacBook-Neo.local>
2026-06-30 19:53:29 +0530

    datatypebasics

---
+++ 01_basic/04_datatypebasic.js
@@ -0,0 +1,19 @@
+const score =88;
+const scorevale = 10.3;
+const isloggedin = false;
+const outside = null;
+let username;
+const id = Symbol("189");
+const newid = Symbol("189");
+console.log(id==newid);
+//array
+const name = ["mukesh","newname","doga"]
+//object
+let myobjct ={
+    name:"muku",
+    age:90,
+}
+const myfunction = function (){
+    console.log("Hellow wolrd");
+}
+console.log(typeof outside );
