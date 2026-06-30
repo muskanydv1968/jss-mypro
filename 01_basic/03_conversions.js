47f99d41df8d4c2757c36dc235b04d06da62b39f
Muskan <muskan@Muskans-MacBook-Neo.local>
2026-06-30 19:09:48 +0530

    conversion file

---
+++ 01_basic/03_conversions.js
@@ -0,0 +1,14 @@
+let score = "33abc";
+// console.log(typeof score);
+let converinto = Number(score);
+console.log(typeof converinto);
+console.log(converinto);
+
+let converbool = "myname";
+let islogedin = Boolean(converbool);
+console.log(islogedin);
+
+let somename = 99;
+let stringname = String(somename);
+console.log(stringname);
+console.log(typeof stringname);
