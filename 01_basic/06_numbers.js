9d645815597bfeb1e537c0712ca7895ad4cda4a5
Muskan <muskan@Muskans-MacBook-Neo.local>
2026-07-01 14:32:14 +0530

    numbers and math

---
+++ 01_basic/06_numbers.js
@@ -0,0 +1,21 @@
+const score = 400;
+const balance = new Number(100);
+console.log(balance.toString().length);
+console.log(score);
+console.log(balance.toFixed(3));
+
+const othernumb = 100.8767;
+console.log(balance.toPrecision(4));
+const hundreds = 10000000;
+console.log(hundreds.toLocaleString("en-IN"));
+console.log(Math);
+console.log(Math.abs(-7));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.9));
+console.log(Math.round(4.6));
+console.log(Math.min(9,3,5,7));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1)+min));
