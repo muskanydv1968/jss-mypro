49d74ec92e2f7f5c28b8d822adbd2ba8720aa3f0
Muskan <muskan@Muskans-MacBook-Neo.local>
2026-07-04 20:47:41 +0530

    scops

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,18 @@
+if(true){
+    let a=10
+    const b = 30
+    var c=90
+    // console.log("inner :",a);
+}
+  if (true) {
+   const  username= "muskan"
+   if(username == "muskan") {
+    const website ="youtube"
+    console.log(username+website);
+   }
+}
+console.log(addtwo(8))
+function addtwo(num){
+    return num+1
+}
+// addtwo(9)
