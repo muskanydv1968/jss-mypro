dded33b8023fe9a5a204fa7363fe3522c5dcce4f
Muskan <muskan@Muskans-MacBook-Neo.local>
2026-07-08 22:10:09 +0530

    switch

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,18 @@
+const month= 3;
+switch(month){
+    case 1:
+        console.log("march");
+        break;
+        case 2:
+            console.log("january");
+            break;
+        case 3:
+            console.log("januaryyy");
+            // break;
+        case 4:
+            console.log("januarynew");
+            break;
+            default:
+                console.log("may");
+                break;
+}
