1abe17ab73341244451ff7518d46f4b9fb80bc93
Muskan <muskan@Muskans-MacBook-Neo.local>
2026-07-08 22:02:14 +0530

    if else

---
+++ 04_controlflow/one.js
@@ -0,0 +1,38 @@
+const isloggedin = true
+if(2=="2"){
+    console.log("executed");
+}
+const temperature =41
+if(temperature === 50){
+    console.log("executed");
+}else{
+    console.log("not executed");
+}
+console.log("new executed");
+// const score =200
+// if(score>100){
+//     let power="fly"
+//     console.log('user powe:${power}');
+// }
+const balance =1000;
+// if(balance >500)console.log("test");
+// if(balance <500){
+//     console.log("less than")
+// }else if(balance <750){
+//     console.log("greater than")
+// }else if(balance <900){
+//     console.log("less than 890")
+// }else{
+//     console.log("not now")
+// }
+
+const userloggedin = true
+const debitcard = true
+const loggedfromgoogle =false
+const loggedfromemail= true
+if(userloggedin && debitcard && 2==3){
+    console.log("allow to buy copy");
+}
+if(loggedfromgoogle ||loggedfromemail){
+    console.log("allow now");
+}
