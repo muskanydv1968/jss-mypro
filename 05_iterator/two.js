08b6eb8d708aaa75d2b81f6e5709ca868b5558c7
Muskan <muskan@Muskans-MacBook-Neo.local>
2026-07-13 22:27:28 +0530

    while loop

---
+++ 05_iterator/two.js
@@ -0,0 +1,17 @@
+let index =0;
+while(index<=10){
+    console.log( index);
+    index=index+2;
+}
+// let myarray =["batman","newone","nownow"];
+// let arr=0;
+// while(arr<myarray.length){
+//     console.log(myarray[arr]);
+//     arr =arr+1;
+// }
+let score =0
+do{
+    console.log(score);
+    score = score+2;
+    
+}while(score<=10);
