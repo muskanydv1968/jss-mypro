76bff853964b21adb133ba61ca49dd276bbfb9ac
Muskan <muskan@Muskans-MacBook-Neo.local>
2026-07-13 22:52:56 +0530

    four

---
+++ 05_iterator/four.js
@@ -0,0 +1,15 @@
+const myobject ={
+    js:"javascript",
+    cpp:"cppp",
+    new:"notnow"
+}
+// for (const key in myobject) {
+//     console.log(key);
+// }
+const programmming =["cp","js","newone"];
+for (const key in programmming) {
+    console.log(programmming[key])
+    
+    
+    
+}
